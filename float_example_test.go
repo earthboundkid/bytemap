58b122e27f554cf3c210d7e4777e50eb516198a8
Carl Johnson <me@carlmjohnson.net>
2023-01-28 16:10:13 -0500

    ExampleFloat_SetFrequencies: Use moby-dick.txt for fun

--- float_example_test.go
+++ float_example_test.go
@@ -3,49 +3,38 @@ package bytemap_test
 import (
 	"fmt"
 	"io"
-	"strings"
+	"os"
 
 	"github.com/carlmjohnson/bytemap"
 )
 
 func ExampleFloat_SetFrequencies() {
+	f, err := os.Open("testdata/moby-dick.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
 	var freqmap bytemap.Float
-	r := strings.NewReader(`The quick brown fox jumps over the lazy dog.`)
-	_, _ = io.Copy(&freqmap, r)
+	_, _ = io.Copy(&freqmap, f)
 	freqmap.SetFrequencies()
 	for _, freq := range freqmap.MostCommon() {
-		if freq.N > 0 {
+		if freq.N > 0.02 {
 			fmt.Printf("%q: %04.1f%%\n", []byte{freq.Byte}, freq.N*100)
 		}
 	}
 	// Output:
-	// " ": 18.2%
-	// "o": 09.1%
-	// "e": 06.8%
-	// "h": 04.5%
-	// "r": 04.5%
-	// "u": 04.5%
-	// ".": 02.3%
-	// "T": 02.3%
-	// "a": 02.3%
-	// "b": 02.3%
-	// "c": 02.3%
-	// "d": 02.3%
-	// "f": 02.3%
-	// "g": 02.3%
-	// "i": 02.3%
-	// "j": 02.3%
-	// "k": 02.3%
-	// "l": 02.3%
-	// "m": 02.3%
-	// "n": 02.3%
-	// "p": 02.3%
-	// "q": 02.3%
-	// "s": 02.3%
-	// "t": 02.3%
-	// "v": 02.3%
-	// "w": 02.3%
-	// "x": 02.3%
-	// "y": 02.3%
-	// "z": 02.3%
+	// " ": 15.5%
+	// "e": 09.3%
+	// "t": 06.9%
+	// "a": 06.0%
+	// "o": 05.5%
+	// "n": 05.2%
+	// "i": 05.0%
+	// "s": 05.0%
+	// "h": 04.9%
+	// "r": 04.1%
+	// "l": 03.3%
+	// "d": 03.0%
+	// "u": 02.1%
 }
